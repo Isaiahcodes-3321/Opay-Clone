1d285236c30649f3377d7b36d751a4d13e2ac26d
Isaiahcodes-3321 <shellisaiah2020@gmail.com>
2026-04-11 02:56:50 +0400

    footer update

--- src/Screens/footer/FooterView.jsx
+++ src/Screens/footer/FooterView.jsx
@@ -1,8 +1,20 @@
+import '../../pages_styling/footer_styling.css'
+import footerLogo1 from '../../assets/footer-logo.png';
+import footerlogoImg2 from '../../assets/logoImg.png';
+
 
 export function FooterBody(){
     return (
-        <footer>
-            
+        <footer className="footerBody">
+            <div className='footerChildBody1'>
+                {/* for bigger screen */}
+                <img className='logoBgScreen' src= {footerLogo1} alt="" />
+                {/* for smaller screen */}
+                <img className='logoSmScreen' src={footerlogoImg2} alt="" />
+            </div>
+            <div className='footerChildBody2'>
+
+            </div>
         </footer>
     );
 }
