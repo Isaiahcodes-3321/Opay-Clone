8f7f857c8014c9fa3c6b3d80d4ffd0e4ed9c1ed5
Isaiahcodes-3321 <shellisaiah2020@gmail.com>
2026-04-13 18:22:32 +0400

    abput page update

--- src/Screens/AboutUsScreen.jsx
+++ src/Screens/AboutUsScreen.jsx
@@ -1,13 +1,36 @@
 import { FooterBody } from "./footer/FooterView";
+import '../pages_styling/export_pages_main.css'
+import img1 from '../assets/img1.png'
+import img2 from '../assets/img2.png'
+
+
 
 export function AboutUsSectionOne() {
     return (
         <>
-            <div className="">
-
+            <div className="aboutPageMainBody">
+                <h1>Company Values</h1>
+                <ValueComponent text1='Entrepreneurship' text2="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid consequuntur commodi cumque magnam odit," img={img2} vcParentBoxReStyle="reStyleBox1"  padd="rh22"/>
+                <ValueComponent text1='Diversity' text2="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid consequuntur commodi cumque magnam odit," img={img1} vcParentBoxReStyle="reStyleBox2" padd="rh2"/>
+                <ValueComponent text1='Innovation' text2="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid consequuntur commodi cumque magnam odit," img={img2} vcParentBoxReStyle="reStyleBox3" padd="rh22"/>
+                <ValueComponent text1='Community' text2="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid consequuntur commodi cumque magnam odit," img={img1} vcParentBoxReStyle="reStyleBox4" padd="rh2"/>
             </div>
             <FooterBody />
         </>
 
+    );
+}
+
+export function ValueComponent({ text1, text2, img, vcParentBoxReStyle = '', padd = '' }) {
+    return (
+        <div className={`vcParentBox ${vcParentBoxReStyle}`}>
+            <div className="vcChildBox1">
+                <h2 className={`${padd}`}>{text1}</h2>
+                <h3 className={`${padd}`}>{text2}</h3>
+            </div>
+            <div className="vcChildBox1">
+                <img src={img} alt="" />
+            </div>
+        </div>
     );
 }
--- src/Screens/SolutionScreen.jsx
+++ src/Screens/SolutionScreen.jsx
@@ -1,13 +1,48 @@
+import '../pages_styling/export_pages_main.css'
 import { FooterBody } from "./footer/FooterView";
+import { ButtonComponent } from '../components/UiComponents'
+import img4 from '../assets/img4.png'
+
+
 
 export function SolutionSection() {
-     return (
-           <>
-               <div className="">
-   
-               </div>
-               <FooterBody />
-           </>
-   
-       );
+    return (
+        <>
+            <div className="solutionPageMainBody">
+                <div className='parentBoxSection2'>
+                    <Section2SolutionView imgPath={img4} text1='Digitize valuechains' text2="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid consequuntur commodi cumque, sit amet elit." />
+                    <Section2SolutionView imgPath={img4} text1='Automate payment collection processes' text2="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid consequuntur commodi cumque, sit amet elit." />
+                    <Section2SolutionView imgPath={img4} text1='Eliminate potential business risks' text2="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid consequuntur commodi cumque, sit amet elit." />
+                    <Section2SolutionView imgPath={img4} text1='Expand saleschannels' text2="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid consequuntur commodi cumque, sit amet elit." />
+                    <Section2SolutionView imgPath={img4} text1='Enhance customer loyalty and improve repeat purchase rate' text2="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid consequuntur commodi cumque, sit amet elit." />
+
+                </div>
+                <Section3SolutionView />
+            </div>
+            <FooterBody />
+        </>
+
+    );
+}
+
+export function Section2SolutionView({ imgPath, text1, text2 }) {
+    return (
+        <div className='boxChildSection2'>
+            <div className='imgView'> <img src={imgPath} alt="" /></div>
+            <h2>{text1}</h2>
+            <h3>{text2}</h3>
+        </div>
+    );
+}
+
+
+export function Section3SolutionView() {
+    return (
+        <>
+            <ButtonComponent buttonText="Contact Opay" className='sectionFourContactButton'>
+                <i className="fas fa-phone" />
+            </ButtonComponent>
+            <h2 className='lastText'>Get in touch with us to learn more</h2>
+        </>
+    );
 }
